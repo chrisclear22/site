44d8773dc3e7a6832b00effcd51127a2a52df4dc
qaziislam <qazi.r.islam@gmail.com>
2015-01-14 10:35:58 -0600

    Boxes and stuff

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Home</title>
+    <link rel="shortcut icon" href="_images/leaptrip_logo/leaptrip.ico" type="image/x-icon" />
    <link href="css/bootstrap.min.css" rel="stylesheet">
    <link rel="stylesheet" type="text/css" href="css/full-slider.css">
    <link rel="stylesheet" type="text/css" href="css/custom.css">
--- slider.php
+++ slider.php
@@ -3,16 +3,16 @@
         <div class="carousel-inner">
             <div class="item active">
                 <!-- Set the first background image using inline CSS below. -->
-                <div><img src="_images/bora_bora.jpg" alt="bora bora"></div>
+                <div><img src="_images/paris/paris5.jpg"></div>
                 <div class="carousel-caption">
-                    <h2>Bora Bora</h2>
+                    <h2>Paris</h2>
                     <p>FROM</p>
                     <p>$1,500</p>
                 </div>
             </div>
             <div class="item">
                 <!-- Set the second background image using inline CSS below. -->
-                <div><img src="_images/hawaii_kauai3.jpg" alt="kauai"></div>
+                <div><img src="_images/kauai/hawaii_kauai2.jpg"></div>
                 <div class="carousel-caption">
                     <h2>Kauai</h2>
                     <p>FROM</p>
@@ -21,9 +21,9 @@
             </div>
             <div class="item">
                 <!-- Set the third background image using inline CSS below. -->
-                <div><img src="_images/venice_italy3.jpg" alt="venice italy"></div>
+                 <div><img src="_images/bora_bora/bora_mask.jpg"></div>
                 <div class="carousel-caption">
-                    <h2>Venice</h2>
+                    <h2>Bora Bora</h2>
                     <p>FROM</p>
                     <p>$1,500</p>
                 </div>
