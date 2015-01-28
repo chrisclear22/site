93b72b449a33c286b9172e87c0b478c3c243a384
qaziislam <qazi.r.islam@gmail.com>
2015-01-28 10:50:38 -0600

    hiuihu

--- index 2.php
+++ index 2.php
@@ -1,14 +1,22 @@
 <?php $page_title="Leap Trip | Home"; ?>
 <?php include("head.php"); ?>
+
 <body>
-<?php include ("analyticstracking.php"); ?>
+	<?php include ("analyticstracking.php"); ?>
+	<!-- Start of NavBar -->
 <?php include("navbar_min.php"); ?>
+	<!-- End of NavBar -->
+
+	<!-- Start of Slider -->
 <?php include("slider_min.php"); ?>
+    <!-- END OF SLIDER -->
+
+<!-- Start of First Row -->
 <div class="index_main_content_wrapper">
 <section id="intro" data-speed="6" data-type="background">
-<div class="container imgs top">
-<div class="row">
-<?php
+    <div class="container imgs top">
+        <div class="row">
+            <?php
                 require('mysqli_connect.php');
                 $q = "SELECT destination_id AS id, image, title, price, alt FROM destinations WHERE destination_id <= 6";
                 $result = mysqli_query($dbcon, $q);
@@ -27,27 +35,35 @@
     
                     echo '<p>' . mysqli_error($dbcon) . '<br><br />Query: ' . $q . '</p>';
                     } 
-                    ; ?>
-</div>
-</div>
-</div>
+                    ; ?> 
+            </div>
+        </div>
+        
+        </div>
 </section>
-<div class="container featured">
-<div class="row">
-<div class="col-lg-4 col-med-4 col-sm-12 col-xs-12">
-<img class="center-block img-responsive img-circle" src="clients/thumbs_up.jpg" alt="client image">
-</div>
-<div class="col-lg-8 col-med-8 col-sm-12 col-xs-12">
-<blockquote class="gigantic center-block">
-<p>This site is so awesome! It was very simple and easy to navigate through and the prices are fantastic</p><footer>Melissa Mathers<cite> Mother of 2</cite></footer>
-</blockquote>
-</div>
-</div>
-</div>
+   <!-- End of First Row -->
+ 
+   <!-- Start of Second Row -->     
+        <div class="container featured">
+        <div class="row">
+            <div class="col-lg-4  col-med-4 col-sm-12 col-xs-12">
+                <img class="center-block img-responsive img-circle" src="clients/thumbs_up.jpg" alt="client image">
+            </div>
+            <div class="col-lg-8 col-med-8 col-sm-12 col-xs-12">
+                <blockquote class="gigantic center-block">
+                    <p>This site is so awesome! It was very simple and easy to navigate through and the prices are fantastic</p><footer>Melissa Mathers<cite> Mother of 2</cite></footer>
+                </blockquote>
+            </div>
+        </div>
+        </div>
+
+    <!-- End of Second Row -->
+
+    <!-- Start of Third Row -->
 <section id="about" data-speed="2" data-type="background">
-<div class="container imgs bottom">
-<div class="row">
-<?php
+        <div class="container imgs bottom">
+        <div class="row">
+            <?php
                 
                 $q = "SELECT destination_id AS id, image, title, price, alt FROM destinations WHERE destination_id BETWEEN 7 AND 9";
                 $result = mysqli_query($dbcon, $q);
@@ -66,11 +82,16 @@
     
                     echo '<p>' . mysqli_error($dbcon) . '<br><br />Query: ' . $q . '</p>';
                     } 
-                    mysqli_close($dbcon); ?>
-</div>
-</div>
+                    mysqli_close($dbcon); ?> 
+
+        </div>
+        </div>
 </section>
 </div>
+    <!-- End of Third Row -->
+
+    <!-- Start of Footer -->
 <?php include("footer_min.php"); ?>
+	<!-- End of Footer -->
 </body>
 </html>
