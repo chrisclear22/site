b3e9199101c8397b3debcff3786c7b0f52fa924f
Chris Clear <chrisclear22@gmail.com>
2015-01-28 11:05:21 -0600

    sync

--- video_min.php
+++
@@ -1,1 +0,0 @@
- <header id="video"><video controls poster="_images/leaptrip_logo/video_logo.png"> <source src="video/leaptrip-_2.mp4" type="video/mp4"> <source src="video/leaptrip-_2.ogv" type="video/ogv"> <source src="video/leaptrip-_2.m4v" type="video/m4v"> <source src="video/leaptrip-_2.webm" type="video/webm">Your browser does not support the video tag.</video></div></header>
