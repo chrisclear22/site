6cf4c51e3c37e0f1a00bee805a1f2ed74cf911ba
Chris Clear <chrisclear22@gmail.com>
2015-03-23 08:47:36 -0500

    Made slider image clickable to navigate to correct place,

--- slider.php
+++ slider.php
@@ -3,31 +3,31 @@
         <div class="carousel-inner">
             <div class="item active">
                 <!-- Set the first background image using inline CSS below. -->
-                <div><img src="_images/paris/paris5.jpg" alt="slide of paris"></div>
+                <a href="destination.php?id=7"><div><img src="_images/paris/paris5.jpg" alt="slide of paris"></div>
                 <div class="carousel-caption">
                     <h1 class="font">Paris</h1>
                     <h4>FROM</h4>
                     <h2>$3,680</h2>
-                </div>
+                </div></a>
             </div>
             <div class="item">
                 <!-- Set the second background image using inline CSS below. -->
-                <div><img src="_images/kauai/hawaii_kauai2.jpg" alt="slide of kauai"></div>
+                <a href="destination.php?id=4"><div><img src="_images/kauai/hawaii_kauai2.jpg" alt="slide of kauai"></div>
                 <div class="carousel-caption">
                     <h1 class="font">Kauai</h1>
                     <h4>FROM</h4>
                     <h2>$2,200</h2>
-                </div>
+                </div></a>
             </div>
             <div class="item">
                 <!-- Set the third background image using inline CSS below. -->
-                 <div><img src="_images/bora_bora/bora_mask.jpg" alt="slide of bora bora"></div>
+                <a href="destination.php?id=1"><div><img src="_images/bora_bora/bora_mask.jpg" alt="slide of bora bora"></div>
                 <div class="carousel-caption">
                     <h1 class="font">Bora Bora</h1>
                     <h4>FROM</h4>
                     <h2>$2,250</h2>
                 </div>
-            </div>
+            </div></a>
         </div>
 
         <!-- Controls -->
